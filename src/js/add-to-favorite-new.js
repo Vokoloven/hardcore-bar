9fdd5d30f3fee140c8c16fac6761fd801dfc596e
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-04 15:15:51 +0300

    r
    r

--- src/js/add-to-favorite-new.js
+++ src/js/add-to-favorite-new.js
@@ -1,24 +1,24 @@
-// const catalogueListRef = document.querySelector('.catalogue__list');
+const catalogueListRef = document.querySelector('.catalogue__list');
 
-// if (catalogueListRef) {
-//   catalogueListRef.addEventListener('mousedown', onClickBtn);
+if (catalogueListRef) {
+  catalogueListRef.addEventListener('mousedown', onClickBtn);
 
-//   function onClickBtn(e) {
-//     if (e.target.classList.contains('btn__orange')) {
-//       //   console.log('hi');
-//       console.dir(catalogueListRef);
-//       const backdropSerg = document.querySelector(
-//         'body > main > div.main-page > section.catalogue.container > div > div.backdrop'
-//       );
+  function onClickBtn(e) {
+    if (e.target.classList.contains('btn__orange')) {
+      //   console.log('hi');
+      console.dir(catalogueListRef);
+      const backdropSerg = document.querySelector(
+        'body > main > div.main-page > section.catalogue.container > div > div.backdrop'
+      );
 
-//       if (backdropSerg) {
-//         backdropSerg.addEventListener('click', onClickSergModal);
-//       }
-//       function onClickSergModal(e) {
-//         if (e.target.classList.contains('modal-first__button')) {
-//           console.log('hi');
-//         }
-//       }
-//     }
-//   }
-// }
+      if (backdropSerg) {
+        backdropSerg.addEventListener('click', onClickSergModal);
+      }
+      function onClickSergModal(e) {
+        if (e.target.classList.contains('modal-first__button')) {
+          console.log('hi');
+        }
+      }
+    }
+  }
+}
